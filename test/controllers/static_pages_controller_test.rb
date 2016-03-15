d49b640aa144b1e48493115a781f19b2352bfc1b
neutligen <neutligen@gmail.com>
2016-03-15 16:13:08 -0400

    静的ページの配置完了

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Habitize | ホーム" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Habitize | ヘルプ" 
   end
 
+  test "aboutページが表示される" do
+  	get :about
+  	assert_response :success
+  	assert_select "title","Habitize | ハビタイズとは" 
+  end
 end
