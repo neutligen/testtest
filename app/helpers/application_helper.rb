97975847dd5c385ff62f3b26b4b5c212c4033f1f
neutligen <neutligen@gmail.com>
2016-03-22 08:53:54 -0400

    review chap4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # ページのタイトルがなければ、デフォルトタイトルを表示する
+  def full_title(page_title = '')
+	  base_title = "Habitize"
+	  if page_title.empty?
+		  base_title
+	  else
+		  base_title + " | " + page_title
+	  end
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,30 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   def setup
-  	@base_title = "Habitize |"
+  	@base_title = "Habitize"
   end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","#{@base_title} ホーム" 
+    assert_select "title","#{@base_title}" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","#{@base_title} ヘルプ" 
+    assert_select "title","#{@base_title} | ヘルプ" 
   end
 
   test "aboutページが表示される" do
   	get :about
   	assert_response :success
-  	assert_select "title","#{@base_title} ハビタイズとは" 
+  	assert_select "title","#{@base_title} | ハビタイズとは" 
   end
 
   test "問い合わせページが表示される" do
   	get :contact
   	assert_response :success
-  	assert_select "title", "#{@base_title} お問い合わせ"
+  	assert_select "title", "#{@base_title} | お問い合わせ"
   end
 end
