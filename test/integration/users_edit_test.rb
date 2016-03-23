372793a0017ebd065b0609c255cbe761f6ae5229
neutligen <neutligen@gmail.com>
2016-03-23 04:15:57 -0400

    ヘッダーパシャルから右記を除外 <li class=photo_layout><%= link_to gravatar_for(@user, size: 40), current_user %></li>

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,7 +8,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "不正な更新" do
   	current_user = users(:michael)
-  	get edit_user_path(current_user)
+  	get edit_user_path(@user)
   	assert_template 'users/edit'
   	patch user_path(@user), user:{email: "foo@invalid", password: "foo", password_confirmation: "bar" }
   	assert_template 'users/edit'
