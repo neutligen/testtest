26fd03fa7493616471939263bc08a90b46c16c6c
neutligen <neutligen@gmail.com>
2016-03-23 04:11:30 -0400

    keep right above pict

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+  	@user = users(:michael)
+  end
+
+  test "不正な更新" do
+  	current_user = users(:michael)
+  	get edit_user_path(current_user)
+  	assert_template 'users/edit'
+  	patch user_path(@user), user:{email: "foo@invalid", password: "foo", password_confirmation: "bar" }
+  	assert_template 'users/edit'
+  end
+
+end
