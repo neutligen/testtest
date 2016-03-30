b183d39781682ff4dde72a77ff19fd88df575132
neutligen <neutligen@gmail.com>
2016-03-30 19:09:37 -0400

    pg struggle

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+	has_many :to_do_lists, dependent: :destroy
   belongs_to :user
   validates :user_id, presence: true
   validates :category_name, presence: true
