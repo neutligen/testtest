b28f6086995b88eb9ac530e62975c89bb39d2265
neutligen <neutligen@gmail.com>
2016-03-30 23:45:30 -0400

    hide valid_user2

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,7 +6,7 @@ class AccountActivationsController < ApplicationController
 			user.activate
 			log_in user
 			flash[:info] = "ありがとうございます！　アカウントが有効化されました。"
-			redirect_to user
+			redirect_to root_url
 		else
 			flash[:danger] = "無効な操作です。"
 			redirect_to root_url
