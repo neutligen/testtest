349a504681a06034edc22dc59d532f9b76e48714
neutligen <neutligen@gmail.com>
2016-03-31 16:43:04 -0400

    fix comp, lugg, headerPictLink

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
 			size = option[:size]
 			gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 			# image_tag(gravatar_url, alt: user.email[/[a-z\d\w\-.]+/,0],class:"gravatar")
-			link_to(image_tag(gravatar_url, alt: user.email[/[a-z\d\w\-.]+/,0],class:"gravatar"), current_user) 
+			link_to(image_tag(gravatar_url, alt: user.email[/[a-z\d\w\-.]+/,0],class:"gravatar"), to_do_lists_path) 
 		else
 			link_to(image_tag("wadachi_2_icon.jpg", alt:"wadachi_2_icon.jpg"), "http://www.i-noh.com")
 		end
