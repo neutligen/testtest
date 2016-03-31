f99f21979931ddd6e97a40f2aaaee78e36bce032
neutligen <neutligen@gmail.com>
2016-03-31 18:43:20 -0400

    remove ssl instance

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # 納期及び、ユーザ数から考えて証明書を購入することができないので、一時的に除外。。。_20160408
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
