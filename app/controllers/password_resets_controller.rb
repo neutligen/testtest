134006cecb8ad750e3f2f2b8a0217942a8b1f4ed
neutligen <neutligen@gmail.com>
2016-03-30 23:19:22 -0400

    help_me

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -49,7 +49,8 @@ class PasswordResetsController < ApplicationController
 
     # 正しいユーザを確認する
     def valid_user
-      unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
     end
